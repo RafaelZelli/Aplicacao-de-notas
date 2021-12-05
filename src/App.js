098aece48bdc0f2bc4fdb46f7e9d10fbacc64c23
Rafael Zelli <rafazelli@gmail.com>
2021-12-05 19:49:45 -0300

    Estilizando componentes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-
-
-class App extends Component  {
-  render(){
-  return (
-    <section>
-      <FormularioCadastro />
-      <ListaDeNotas />
-    </section>
-  );
- }
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
